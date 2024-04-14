155c015e0706636f499756f6f03513bbc9544cd7
olgaIsFF <olga.isfff@gmail.com>
2024-04-14 04:07:08 +0300

    Добавление страницы регистрации

--- UnitTest/AuthPage.xaml.cs
+++ UnitTest/AuthPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.IO.Packaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,62 @@ namespace UnitTest
         public AuthPage()
         {
             InitializeComponent();
+        }
+
+        private void ButtonEnter_Click(object sender, RoutedEventArgs e)
+        {
+            Auth(TextBoxLogin.Text, TextBoxPassword.Text);
+
+        }
+
+        private void ButtonReg_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("RegPage.xaml", UriKind.Relative));
+        }
+
+        private int failedAttempts = 0;
+        public bool Auth(string login, string password)
+        {
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Введите логин и пароль");
+                return false;
+            }
+
+            using (var db = new Entities())
+            {
+                var user = db.User
+                    .AsNoTracking()
+                    .FirstOrDefault(u => u.Login == login && u.Password == password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Пользователь с такими данными не найден");
+                    failedAttempts++;
+
+                    if (failedAttempts >= 3)
+                    {
+                        // Открытие окна капчи
+                        CaptchaWindow captchaWindow = new CaptchaWindow();
+                        captchaWindow.ShowDialog();
+                        failedAttempts = 0;
+                    }
+
+                    return false;
+                }
+
+                if (user.Role == "Удален")
+                {
+                    MessageBox.Show("Пользователь с такими данными удалён");
+                    return false;
+                }
+                MessageBox.Show($"Здравствуйте, {user.Role} {user.FIO}!");
+                TextBoxLogin.Clear();
+                TextBoxPassword.Clear();
+
+                return true;
+
+            }
         }
     }
 }
---
+++ UnitTestProject/UnitTest3.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTest;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest3
+    {
+        [TestMethod]
+        public void AuthTestSuccess()
+        {
+            var page = new AuthPage();
+            Assert.IsTrue(page.Auth("Elizor@gmai,com", "yntiRS"));
+            Assert.IsTrue(page.Auth("Vladlena@gmai.com", "07i7Lb"));
+            Assert.IsTrue(page.Auth("Adam@gmai.com", "7SP9CV"));
+            Assert.IsTrue(page.Auth("Yli@gmai.com", "GwffgE"));
+            Assert.IsTrue(page.Auth("Vasilisa@gmai.com", "d7iKKV"));
+            Assert.IsTrue(page.Auth("Galina@gmai.com", "8KC7wJ"));
+            Assert.IsTrue(page.Auth("Miron@@gmai,com", "x58OAN"));
+            Assert.IsTrue(page.Auth("Vseslava@gmai.com", "fhDSBf"));
+            Assert.IsTrue(page.Auth("Anisa@gmai.com", "Wh4OYm"));
+            Assert.IsTrue(page.Auth("Feafan@@gmai,com", "Kc1PeS"));
+        }
+
+        [TestMethod]
+        public void AuthTestNegative()
+        {
+            var page = new AuthPage();
+            Assert.IsFalse(page.Auth("Kar@gmai.com", "6QF1WB"));
+            Assert.IsFalse(page.Auth("Victoria@gmai.com", "9mlPQJ"));
+            Assert.IsFalse(page.Auth("Yli@gmai.com", "8KC7wJ"));
+            Assert.IsFalse(page.Auth("olaf@lk.ru", "dsfgg"));
+            Assert.IsFalse(page.Auth("", ""));
+        }
+
+        [TestMethod]
+        public void AuthTestCaptcha()
+        {
+            var page = new AuthPage();
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsFalse(page.Auth("Yli@gmai.com", "123"));
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Assert.IsFalse(page.Auth("1234", "sldkfl"));
+            }
+        }
+    }
+}
